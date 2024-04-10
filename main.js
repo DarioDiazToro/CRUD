652366c314e049469761864fbecb2d345ce11874
Dario <haiverdiaztoro@gmail.comgit config --listgit config --global user.email haiverdiaztoro@gmail.com>
2024-04-10 13:44:27 -0500

    comentar importacion de UserApp del archivo main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import "./style.css";
-import { UserApp } from "./src/users/users-app";
+// import { UserApp } from "./src/users/users-app";
 
 document.querySelector("#app").innerHTML = `
-  <div>
     <h1 id ="app-title">Hello Vite!</h1>
     <div class="card">
     </div>
--- src/users/users-app.js
+++ src/users/users-app.js
@@ -3,32 +3,23 @@ import { renderButtons } from "./presentation/render-buttons/render-buttons";
 import { modal, renderModal } from "./presentation/render-modal/render-modal";
 import { renderTable } from "./presentation/render-table/renderTable";
 import usersStore from "./store/users-store";
-import {  saveUser, updateUser } from "./uses-cases/save-user";
+import { saveUser } from "./uses-cases/save-user";
 
-
-
- /**
-  * 
-  * @param {HTMLDivElement} element 
-  */
- export const UserApp = async (element)=>{
-
-    element.innerHTML = "Loading...";
-    await usersStore.loadNextPage();
-      usersStore.loadPreviusPage();
-    element.innerHTML = "";
-    renderTable(element)
-    renderButtons(element);
-    renderAddButton(element);
-    renderModal(element,async(userLike)=>{ 
-  
-      const user = await saveUser(userLike);
-      usersStore.onUserChanged(user);
-      renderTable();
-    });
-
-
- };
-
-
-
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const UserApp = async (element) => {
+  element.innerHTML = "Loading...";
+  await usersStore.loadNextPage();
+  usersStore.loadPreviusPage();
+  element.innerHTML = "";
+  renderTable(element);
+  renderButtons(element);
+  renderAddButton(element);
+  renderModal(element, async (userLike) => {
+    const user = await saveUser(userLike);
+    usersStore.onUserChanged(user);
+    renderTable();
+  });
+};
